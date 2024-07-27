fb694d8c7f7aa184254afdef0a83356cf6c57d5e
sachingahlot21 <gahlotsachin69@gmail.com>
2024-07-27 23:51:19 +0530

    music_academy

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+
+function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
